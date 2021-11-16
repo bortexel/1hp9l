166bb6396edb5b283422b30b0ed02c1007312000
RuscalWorld <me@ruscalworld.ru>
2021-11-16 22:29:08 +0300

    Initial commit. Но зачем...

---
+++ src/main/java/ru/bortexel/hardcore/BortexelHardcore.java
@@ -0,0 +1,10 @@
+package ru.bortexel.hardcore;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class BortexelHardcore implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
